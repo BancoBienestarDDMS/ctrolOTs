9366f2216748e9030431a63f70f9b5702e3ed789
Beto <apinzon@outlook.com>
2020-02-11 19:50:22 -0600

    formateando fecha en datatable

--- js/tabla_ots.js
+++ js/tabla_ots.js
@@ -9,28 +9,67 @@ $(document).ready(function() {
     format: "json"
   }).done(function( data ) {
       var datos = data;
-      // console.log('done datos = ' + data[0].id);
-      $.each( data, function( key, val ) {
-        console.log('Direccion Adjunta: ' + key + ' id: ' + val.id + ' abrev: ' + val.abrev + ' nombre: ' + val.nombre);
-      });
+      console.log('getDAs done ' + datos.length + ' rows');
+      // $.each( data, function( key, val ) {
+      //   console.log('Direccion Adjunta: ' + key + ' id: ' + val.id + ' abrev: ' + val.abrev + ' nombre: ' + val.nombre);
+      // });
     });
 
 
   var ctrl_ots_API_OTs = "http://172.26.160.167/api_ctrl_ots/rsConsultaOTs/getOTs?";
   $.getJSON( ctrl_ots_API_OTs, {
-    // tags: "mount rainier",
     tagmode: "any",
     format: "json"
   }).done(function( data ) {
       var datos = data;
-      tt.DataTable({data: datos});
+      console.log('getOTs done ' + datos.length + ' rows');
+      $('#dt_OTs').DataTable( { 
+        data : datos,
+        columns: [{ title: 'OT',               data: 'OT'               },
+                  { title: 'Area Solicitante', data: 'Area_Solicitante' },
+                  { title: 'Solicitante',      data: 'Solicitante'      },
+                  { title: 'Area_Asignacion',  data: 'Area_Asignacion'  },
+                  { title: 'Fecha Solicitud',  data: 'Fecha_Solicitud'  },
+                  { title: 'Programa',         data: 'Programa'         },
+                  { title: 'SubPrograma',      data: 'SubPrograma'      },
+                  { title: 'Tipo Servicio',    data: 'Tipo_Servicio'    },
+                  { title: 'Estatus',          data: 'Estatus'          },
+                  { title: 'Beneficiarios',    data: 'Beneficiarios'    },
+                  { title: 'Monto',            data: 'Monto'            },
+                  { title: 'Rechazos',         data: 'Rechazos'         },
+                  { title: 'Monto_Rechazos',   data: 'Monto_Rechazos'   },
+                ]
+      } );
+
       $.each( data, function( key, val ) {
-        console.log('Orden Trabajo: ' + key + ' OT: ' + val.OT + ' Programa: ' + val.Programa + ' SubPrograma: ' + val.SubPrograma);
+        if (key < 10) {
+          var dateString  = val.Fecha_Solicitud.substr(6);
+          var currentTime = new Date(parseInt(dateString ));
+          var month       = currentTime.getMonth() + 1;
+          var day         = currentTime.getDate();
+          var year        = currentTime.getFullYear();
+          var hours       = currentTime.getHours();
+          var mins        = currentTime.getMinutes();
+          var secs        = currentTime.getSeconds();
+          var date        = day + "/" + month + "/" + year + " " + hours + ":" + mins + ":" + secs;
+          console.log('Orden Trabajo: ' + key + ' OT: ' + val.OT + ' Fecha Sol.: ' + date + ' Programa: ' + val.Programa + ' SubPrograma: ' + val.SubPrograma + ' Tipo Servicio: ' + val.Tipo_Servicio);
+          // if (typeof val.Fecha_Solicitud === 'string' &&  val.Fecha_Solicitud.match(isoDatePattern)) {
+          //   var ff = new Date(val.Fecha_Solicitud); // isostring, so cast to js date
+          //   // console.log('Orden Trabajo: ' + key + ' OT: ' + val.OT + ' Fecha Sol.: ' + ff + ' Programa: ' + val.Programa + ' SubPrograma: ' + val.SubPrograma);
+          //   // console.log('Orden Trabajo: ' + key + ' OT: ' + val.OT + ' Fecha Sol.: ' + ff.toDateString );
+          //   console.log(ff.toDateString());
+          // }
+        }
+        else {
+          return; 
+        }
       });
+      
+      console.log('listo.');
     });
 
   
-
+  
 
   // console.log('asignamos datos= ' + datos); dcx
   // console.log('a la tabla.');
