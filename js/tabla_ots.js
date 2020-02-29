80f38426cccdef7602bc3fd183dd30d2d59f0c45
Beto <apinzon@outlook.com>
2020-02-28 18:50:47 -0600

    termine los filtros, actualizo tabla, configuro btn buscar..

--- js/tabla_ots.js
+++ js/tabla_ots.js
@@ -1,26 +1,145 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  // $('#dt_OTs').DataTable();
-  var tt = $('#dt_OTs')
-  
-  var ctrl_ots_API_Dir_Ajds = "http://172.26.160.167/api_ctrl_ots/rsConsultaOTs/getDAs?";
-  $.getJSON( ctrl_ots_API_Dir_Ajds, {
-    tagmode: "any",
-    format: "json"
-  }).done(function( data ) {
-      var datos = data;
-      console.log('getDAs done ' + datos.length + ' rows');
-      // $.each( data, function( key, val ) {
-      //   console.log('Direccion Adjunta: ' + key + ' id: ' + val.id + ' abrev: ' + val.abrev + ' nombre: ' + val.nombre);
-      // });
-    });
+    // $('#dt_OTs').DataTable();
+    var tt = $('#dt_OTs')
+    
+    //cargamos catálogos
+    console.log("cargamos catálogos");
 
+    var ctrl_ots_API_Dir_Ajds = "http://172.26.160.167/api_ctrl_ots/rsConsultaOTs/getDAs?";
+    $.getJSON( ctrl_ots_API_Dir_Ajds, {
+      tagmode: "any",
+      format: "json"
+    }).done(function( data ) {
+        var datos = data;
+        console.log('getDAs done ' + datos.length + ' rows');
+        // $.each( data, function( key, val ) {
+        //   console.log('Direccion Adjunta: ' + key + ' id: ' + val.id + ' abrev: ' + val.abrev + ' nombre: ' + val.nombre);
+        // });
+      });
 
-  var ctrl_ots_API_OTs = "http://172.26.160.167/api_ctrl_ots/rsConsultaOTs/getOTs?top=100&anio=19&cont=&ot=&id_AS=&id_Sol=&id_AA=&id_Prog=&id_Subprog=&id_TS=&en_tiempo=&id_status=&id_urg=&id_comp=&f_sol=&f_env=&f_atn=&f_ent=";
-  $.getJSON( ctrl_ots_API_OTs, {
-    tagmode: "any",
-    format: "json"
-  }).done(function( data ) {
+    var ctrl_ots_API_Dirs = "http://172.26.160.167/api_ctrl_ots/rsConsultaOTs/getDirs?";
+    $.getJSON( ctrl_ots_API_Dirs, {
+      tagmode: "any",
+      format: "json"
+    }).done(function( data ) {
+        var datos = data;
+        console.log('getDirs done ' + datos.length + ' rows');
+      });
+    
+    var ctrl_ots_API_SubDirs = "http://172.26.160.167/api_ctrl_ots/rsConsultaOTs/getSubDirs?";
+    $.getJSON( ctrl_ots_API_SubDirs, {
+      tagmode: "any",
+      format: "json"
+    }).done(function( data ) {
+        var datos = data;
+        console.log('getSubDirs done ' + datos.length + ' rows');
+      });
+
+    var ctrl_ots_API_Gers = "http://172.26.160.167/api_ctrl_ots/rsConsultaOTs/getGers?";
+    $.getJSON( ctrl_ots_API_Gers, {
+      tagmode: "any",
+      format: "json"
+    }).done(function( data ) {
+        var datos = data;
+        console.log('getGers done ' + datos.length + ' rows');
+      });
+
+    var ctrl_ots_API_Progs = "http://172.26.160.167/api_ctrl_ots/rsConsultaOTs/getProgs?";
+    $.getJSON( ctrl_ots_API_Progs, {
+      tagmode: "any",
+      format: "json"
+    }).done(function( data ) {
+        var datos = data;
+        console.log('getProgs done ' + datos.length + ' rows');
+      });
+
+    var ctrl_ots_API_SubProgs = "http://172.26.160.167/api_ctrl_ots/rsConsultaOTs/getSubProgs?";
+    $.getJSON( ctrl_ots_API_SubProgs, {
+      tagmode: "any",
+      format: "json"
+    }).done(function( data ) {
+        var datos = data;
+        console.log('getSubProgs done ' + datos.length + ' rows');
+      });
+
+    var ctrl_ots_API_TServs = "http://172.26.160.167/api_ctrl_ots/rsConsultaOTs/getTServs?";
+    $.getJSON( ctrl_ots_API_TServs, {
+      tagmode: "any",
+      format: "json"
+    }).done(function( data ) {
+        var datos = data;
+        console.log('getTServs done ' + datos.length + ' rows');
+      });
+
+    var ctrl_ots_API_Urgs = "http://172.26.160.167/api_ctrl_ots/rsConsultaOTs/getUrgs?";
+    $.getJSON( ctrl_ots_API_Urgs, {
+      tagmode: "any",
+      format: "json"
+    }).done(function( data ) {
+        var datos = data;
+        console.log('getUrgs done ' + datos.length + ' rows');
+      });
+
+    var ctrl_ots_API_Compls = "http://172.26.160.167/api_ctrl_ots/rsConsultaOTs/getCompls?";
+    $.getJSON( ctrl_ots_API_Compls, {
+      tagmode: "any",
+      format: "json"
+    }).done(function( data ) {
+        var datos = data;
+        console.log('getCompls done ' + datos.length + ' rows');
+      });
+
+    var ctrl_ots_API_StsOTs = "http://172.26.160.167/api_ctrl_ots/rsConsultaOTs/getStsOTs?";
+    $.getJSON( ctrl_ots_API_StsOTs, {
+      tagmode: "any",
+      format: "json"
+    }).done(function( data ) {
+        var datos = data;
+        console.log('getStsOTs done ' + datos.length + ' rows');
+      });
+
+    var ctrl_ots_API_ArsAsig = "http://172.26.160.167/api_ctrl_ots/rsConsultaOTs/getArsAsig?";
+    $.getJSON( ctrl_ots_API_ArsAsig, {
+      tagmode: "any",
+      format: "json"
+    }).done(function( data ) {
+        var datos = data;
+        console.log('getArsAsig done ' + datos.length + ' rows');
+      });
+
+    var ctrl_ots_API_Usrs = "http://172.26.160.167/api_ctrl_ots/rsConsultaOTs/getUsrs?";
+    $.getJSON( ctrl_ots_API_Usrs, {
+      tagmode: "any",
+      format: "json"
+    }).done(function( data ) {
+        var datos = data;
+        console.log('getUsrs done ' + datos.length + ' rows');
+      });
+
+    var ctrl_ots_API_Pers = "http://172.26.160.167/api_ctrl_ots/rsConsultaOTs/getPers?";
+    $.getJSON( ctrl_ots_API_Pers, {
+      tagmode: "any",
+      format: "json"
+    }).done(function( data ) {
+        var datos = data;
+        console.log('getPers done ' + datos.length + ' rows');
+      });
+
+    var ctrl_ots_API_RelPuesPers = "http://172.26.160.167/api_ctrl_ots/rsConsultaOTs/getRelPuesPers?";
+    $.getJSON( ctrl_ots_API_RelPuesPers, {
+      tagmode: "any",
+      format: "json"
+    }).done(function( data ) {
+        var datos = data;
+        console.log('getRelPuesPers done ' + datos.length + ' rows');
+      });
+
+    var ctrl_ots_API_OTs = "http://172.26.160.167/api_ctrl_ots/rsConsultaOTs/getOTs?top=100&anio=19&cont=&ot=&id_AS=&id_Sol=&id_AA=&id_Prog=&id_Subprog=&id_TS=&en_tiempo=&id_status=&id_urg=&id_comp=&f_sol=&f_env=&f_atn=&f_ent=";
+    $.getJSON( ctrl_ots_API_OTs, {
+      tagmode: "any",
+      format: "json"
+    }).done(function( data ) {
       var datos = data;
       console.log('getOTs done ' + datos.length + ' rows');
       
@@ -58,27 +177,31 @@ $(document).ready(function() {
       $('#dt_OTs').DataTable( { 
         data : datos,
         columns: [{ title: 'Num. OT',                      data: 'OT'                        },
-                  // { title: 'Cont',                         data: 'Cont'                      },
-                  // { title: 'Anio',                         data: 'Anio'                      },
+               // { title: 'Cont',                         data: 'Cont'                      },
+               // { title: 'Anio',                         data: 'Anio'                      },
                   { title: 'Area de Solicitante',          data: 's_Area_Solicitante'        },
                   { title: 'Solicitante',                  data: 's_Solicitante'             },
                   { title: 'Area de Asignacion',           data: 's_Area_Asignacion'         },
-                  { title: 'Fecha de Solicitud',           data: 's_Fecha_Solicitud'         },
+
                   { title: 'Programa',                     data: 's_Programa'                },
                   { title: 'SubPrograma',                  data: 's_SubPrograma'             },
                   { title: 'Tipo de Servicio',             data: 's_Tipo_Servicio'           },
-                  { title: 'Urgencia',                     data: 's_Urgencia'                },
-                  { title: 'Complejidad',                  data: 's_Complejidad'             },
-                  { title: 'Fecha de Envio',               data: 's_Fecha_Envio'             },
+               // { title: 'Urgencia',                     data: 's_Urgencia'                },
+               // { title: 'Complejidad',                  data: 's_Complejidad'             },   
                   { title: 'Estatus',                      data: 's_Estatus'                 },
+
+                  { title: 'Fecha de Solicitud',           data: 's_Fecha_Solicitud'         },
+                  { title: 'Fecha de Envio',               data: 's_Fecha_Envio'             },
+                  { title: 'Fecha de Atencion',            data: 's_Fecha_Atencion'          },
+                  { title: 'Fecha de Entrega',             data: 's_Fecha_Entrega'           },
+
                   { title: 'Beneficiarios',                data: 'Beneficiarios',  searchable: false },
                   { title: 'Monto',                        data: 'Monto',          searchable: false, render: $.fn.dataTable.render.number(',','.',2,'$') },
                   { title: 'Rechazos',                     data: 'Rechazos',       searchable: false },
                   { title: 'Monto de Rechazos',            data: 'Monto_Rechazos', searchable: false, render: $.fn.dataTable.render.number(',','.',2,'$') },
-                  { title: 'Fecha de Atencion',            data: 's_Fecha_Atencion'          },
-                  { title: 'Tiempo de Atencion',           data: 'Tiempo_Atencion'         },
-                  { title: 'En Tiempo',                    data: 'En_Tiempo'                 },
-                  { title: 'Fecha de Entrega',             data: 's_Fecha_Entrega'           },
+               // { title: 'Tiempo de Atencion',           data: 'Tiempo_Atencion'         },
+               // { title: 'En Tiempo',                    data: 'En_Tiempo'                 },
+               
                   { title: 'Descripcion',                  data: 'Descripcion'               },
                   { title: 'Observaciones',                data: 'Observaciones'             },
                   { title: 'Observaciones de Atencion',    data: 'Observaciones_Atencion'    },
@@ -101,3 +224,6 @@ $(document).ready(function() {
   console.log('listo.');
 });
 
+function consultarOTS () {
+  
+}
